2c46d25fb227d974adea4e18d29f5e813fdfe42e
Beni <benik@live.ca>
2022-09-25 21:43:52 -0400

    set up the url path for leads

--- leads/views.py
+++ leads/views.py
@@ -1,3 +1,10 @@
 from django.shortcuts import render
+from django.http import HttpResponse
 
-# Create your views here.
+
+#When you call this home page the browser wil send you a request witth,
+#a whole lot of data coming from the website that you can use
+def home_page(request):
+
+    return HttpResponse('Hello world')
+
