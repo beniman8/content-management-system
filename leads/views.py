4c05e164a05778c9243b87d2f565f9b69ce0808b
Beni <benik@live.ca>
2022-09-26 00:31:32 -0400

    edit lead list and detail view

--- leads/views.py
+++ leads/views.py
@@ -6,7 +6,7 @@ from .models import Lead
 
 #When you call this home page the browser wil send you a request witth,
 #a whole lot of data coming from the website that you can use
-def home_page(request):
+def lead_list(request):
 
     #this will return a queryset of all your leads as a list
     #meaning if you want to display all of the data one by one you need to run
@@ -29,5 +29,22 @@ def home_page(request):
 
     #this will work if you want to get the templates after you sett the template settings to 
     #BASE_DIR / 'templates'
-    return render(request,'index.html',context)
+    return render(request,'leads/lead_list.html',context)
+
+
+def lead_detail(request, pk):
+    #if you want to return a certain value to our function
+    #add it to the function in our case pk. 
+    #now when you type the url of the detail view with the lead/1
+    #the value 1 gets sent back to us if you set up the url to
+    #receive the pk in its route     path('<pk>/', lead_detail),
+
+    # print(pk)
+
+    lead = Lead.objects.get(id=pk)
+
+    context = {
+        'lead':lead,
+    }
+    return render(request,'leads/lead_detail.html',context)
 
