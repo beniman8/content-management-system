64df10db7c8c6b040ba83f75daa1c473550532c4
Beni <benik@live.ca>
2022-09-25 22:24:28 -0400

    Setting up templates

--- leads/views.py
+++ leads/views.py
@@ -6,5 +6,10 @@ from django.http import HttpResponse
 #a whole lot of data coming from the website that you can use
 def home_page(request):
 
-    return HttpResponse('Hello world')
+    #this will work if you want to find the templates in your app directory
+    # return render(request,'leads/home_page.html')
+
+    #this will work if you want to get the templates after you sett the template settings to 
+    #BASE_DIR / 'templates'
+    return render(request,'index.html')
 
